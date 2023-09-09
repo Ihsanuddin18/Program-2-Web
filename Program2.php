ebf7062ec7e11d389dd72d44bc7c35823229af2e
Ihsanuddin18 <mihsan831@gmail.com>
2023-09-09 13:58:29 +0700

    square root php

---
+++ Program2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $akar_kuadrat = sqrt(9);
+    echo "Akar kuadrat dari 9 adalah $akar_kuadrat";
+    ?>
+</body>
+</html>
